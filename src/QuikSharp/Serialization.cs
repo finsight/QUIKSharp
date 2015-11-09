4c2ef9c5e5c5d34a7193da0fb1ae752463b71faa
Alexey Nebotov <anebotov@gmail.com>
2015-11-09 18:50:06 +0300

    отбрасывание незначащих нулей в цене транзакции

--- src/QuikSharp/Serialization.cs
+++ src/QuikSharp/Serialization.cs
@@ -133,6 +133,32 @@ namespace QuikSharp {
     }
 
     /// <summary>
+    /// Serialize Decimal to string without trailing zeros
+    /// </summary>
+    public class DecimalG29ToStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) { return objectType.Equals( typeof(decimal) ); }
+
+        public override object ReadJson(JsonReader reader,
+                                        Type objectType,
+                                         object existingValue,
+                                         JsonSerializer serializer)
+        {            
+            var t = JToken.Load(reader);
+            return t.Value<decimal>();            
+        }
+
+        public override void WriteJson(JsonWriter writer,
+                                       object value,
+                                       JsonSerializer serializer)
+        {
+            decimal d = (decimal)value;
+            var t = JToken.FromObject(d.ToString("G29"));
+            t.WriteTo(writer);
+        }
+    }
+
+    /// <summary>
     /// Convert DateTime to HHMMSS
     /// </summary>
     // ReSharper disable once InconsistentNaming
