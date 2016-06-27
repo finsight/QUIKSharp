e0d73d7173bbe4bb9ce6e629bb0d0194c7377ffb
Pr0phet1c <prodeaner@gmail.com>
2016-06-27 17:34:17 +0300

    Added ISIN to SecurityInfo

--- src/QuikSharp/DataStructures/SecurityInfo.cs
+++ src/QuikSharp/DataStructures/SecurityInfo.cs
@@ -48,6 +48,10 @@ namespace QuikSharp {
         /// Размер лота
         /// </summary>
         public int lot_size { get; set; }
+        /// <summary>
+        /// ISIN-код
+        /// </summary>
+        public string isin_code { get; set; }
         // ReSharper restore InconsistentNaming
     }
 }
