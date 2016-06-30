9011caa1f4ecd8fa4d0155e764e6f453117f0efb
nubick <nubick@gmail.com>
2016-06-30 13:30:16 +0100

    Fixed 'i' variable scope from global to local.

--- src/QuikSharp/lua/qsutils.lua
+++ src/QuikSharp/lua/qsutils.lua
@@ -48,7 +48,8 @@ function split(inputstr, sep)
     if sep == nil then
         sep = "%s"
     end
-    local t={} ; i=1
+    local t={}
+	local i=1
     for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
         t[i] = str
         i = i + 1
