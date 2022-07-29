e7ac838c72836634814fd593b688ba04a3419255
Agremond <mailboxgmn@gmail.com>
2022-07-30 01:14:34 +0300

    добавлена возможность выбора серии опционов: недельная, месячная, квартальная, все серии

---
+++ src/QuikSharp/DataStructures/OptionType.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2014-2020 QUIKSharp Authors https://github.com/finsight/QUIKSharp/blob/master/AUTHORS.md. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
+
+namespace QuikSharp.DataStructures
+{
+    /// <summary>
+    /// Тип опциона
+    /// </summary>
+    public enum OptionType
+    {
+        /// <summary>
+        /// Call option
+        /// </summary>
+        Call,
+
+        /// <summary>
+        /// Put option
+        /// </summary>
+        Put
+    }
+}
