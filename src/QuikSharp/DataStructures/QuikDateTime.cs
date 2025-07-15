2f44930ee6481756cdff13787760acf09dc74b81
Владислав <v.hohlov@hotmail.com>
2025-07-15 12:56:50 +0300

    Microseconds calculate adds to QuikDateTime

--- src/QuikSharp/DataStructures/QuikDateTime.cs
+++ src/QuikSharp/DataStructures/QuikDateTime.cs
@@ -66,8 +66,10 @@ namespace QuikSharp.DataStructures
         /// <returns></returns>
         public static explicit operator DateTime(QuikDateTime qdt)
         {
-            var dt = new DateTime(qdt.year, qdt.month, qdt.day, qdt.hour, qdt.min, qdt.sec, qdt.ms);
-            return dt; //dt.AddTicks(qdt.mcs * 10);
+            var dt = new DateTime(qdt.year, qdt.month, qdt.day, qdt.hour, qdt.min, qdt.sec);
+            // 1 микросекунда = 10 тиков (1000 наносекунд / 100 наносекунд на тик). // приоритет mcs
+            long ticks = qdt.mcs > 0 ? qdt.mcs * 10L : qdt.ms * 1000L;
+            return dt.AddTicks(ticks); // Добавляем вычисленные тики к базовому времени.
         }
 
         /// <summary>
@@ -77,6 +79,11 @@ namespace QuikSharp.DataStructures
         /// <returns></returns>
         public static explicit operator QuikDateTime(DateTime dt)
         {
+            // Вычисляем тики, которые составляют дробную часть текущей секунды. TimeSpan.TicksPerSecond = 10,000,000.
+            // Оператор остатка (%) мгновенно дает нам тики внутри секунды (от 0 до 9,999,999).
+            long fractionalTicks = dt.Ticks % TimeSpan.TicksPerSecond;
+            int totalMicroseconds = (int)(fractionalTicks / 10);
+
             return new QuikDateTime
             {
                 year = dt.Year,
@@ -86,8 +93,9 @@ namespace QuikSharp.DataStructures
                 min = dt.Minute,
                 sec = dt.Second,
                 ms = dt.Millisecond,
-                mcs = 0, // ((int)(dt.TimeOfDay.Ticks) - ((dt.Hour * 60 + dt.Minute) * 60 + dt.Second) * 1000 * 10000) / 10,
-                week_day = (int) dt.DayOfWeek
+                mcs = totalMicroseconds,
+                //week_day = (int) dt.DayOfWeek
+                week_day = (int)dt.DayOfWeek == 0 ? 7 : (int)dt.DayOfWeek // воскресенье  в QUIK — 7
             };
         }
     }
